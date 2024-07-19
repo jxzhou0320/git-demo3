82966c29c13693cc8f3f8b500de3414e6e0b2846
zjx <1163072972@qq.com>
2024-07-19 20:48:11 +0800

    8 commit

--- src/main/java/com/example/gitdemo3/git/gitDemo.java
+++ src/main/java/com/example/gitdemo3/git/gitDemo.java
@@ -2,6 +2,6 @@ package com.example.gitdemo3.git;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("master7");
+        System.out.println("master8");
     }
 }
