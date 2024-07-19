834fa5bcd5fb2fc6fc69f8b2e6edfbc9d90f49a1
jxzhou0320 <2516503099@qq.com>
2024-07-19 21:54:54 +0800

    pull commmit

--- src/main/java/com/example/gitdemo3/git/gitDemo.java
+++ src/main/java/com/example/gitdemo3/git/gitDemo.java
@@ -3,5 +3,6 @@ package com.example.gitdemo3.git;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("master8");
+        System.out.println("pull");
     }
 }
