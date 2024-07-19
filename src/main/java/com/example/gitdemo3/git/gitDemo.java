6c85c183b218d54b1642e0de2e24f93a30206987
zjx <1163072972@qq.com>
2024-07-19 20:54:54 +0800

    hot66 commit

--- src/main/java/com/example/gitdemo3/git/gitDemo.java
+++ src/main/java/com/example/gitdemo3/git/gitDemo.java
@@ -2,6 +2,6 @@ package com.example.gitdemo3.git;
 
 public class gitDemo {
     public static void main(String[] args) {
-        System.out.println("hot-fix_55");
+        System.out.println("hot-fix_66");
     }
 }
